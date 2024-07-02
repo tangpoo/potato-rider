6cb00e29026d80b76a75dbaf8fedabb1b15fbf18
tangpoo <lhinav3@gmail.com>
2024-07-02 21:52:00 +0900

    프로젝트 초기화

---
+++ src/main/java/com/potatorider/potatorider/PotatoRiderApplication.java
@@ -0,0 +1,13 @@
+package com.potatorider.potatorider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoRiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatoRiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/potatorider/potatorider/PotatoRiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatorider.potatorider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoRiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
