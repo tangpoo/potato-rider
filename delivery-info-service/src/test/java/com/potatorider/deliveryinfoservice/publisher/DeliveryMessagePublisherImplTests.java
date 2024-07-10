12a44d0a0c30030ad9f6cd5ccaa77713f550b8ce
tangpoo <lhinav3@gmail.com>
2024-07-10 15:08:33 +0900

    #25 Test: DeliveryPublisher Tests

--- delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/publisher/DeliveryMessagePublisherImplTests.java
+++ delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/publisher/DeliveryMessagePublisherImplTests.java
@@ -1,31 +1,30 @@
 package com.potatorider.deliveryinfoservice.publisher;
 
-import static com.potatorider.domain.DeliveryStatus.*;
-import static org.mockito.Mockito.*;
+import static com.potatorider.domain.DeliveryStatus.ACCEPT;
+import static com.potatorider.domain.DeliveryStatus.REQUEST;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.potatorider.deliveryinfoservice.domain.DeliverySteps;
 import com.potatorider.domain.Delivery;
-import com.potatorider.domain.DeliveryStatus;
 import com.potatorider.publisher.DeliveryMessagePublisherImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
 public class DeliveryMessagePublisherImplTests {
 
-    DeliveryMessagePublisherImpl deliveryPublisher;
-
-    AmqpTemplate amqpTemplate;
-
     private final String shopExchange = "messageQueue.exchange.shop";
     private final String agencyExchange = "messageQueue.exchange.agency";
+    DeliveryMessagePublisherImpl deliveryPublisher;
+    AmqpTemplate amqpTemplate;
 
     @BeforeEach
     void setUp() {
