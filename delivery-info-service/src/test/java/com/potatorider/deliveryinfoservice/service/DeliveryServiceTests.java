3fee084fca5b6f6e58fac69f56bd95e166efcc74
tangpoo <lhinav3@gmail.com>
2024-07-10 15:08:50 +0900

    #22 Test: DeliveryService Tests

---
+++ delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/service/DeliveryServiceTests.java
@@ -0,0 +1,252 @@
+package com.potatorider.deliveryinfoservice.service;
+
+import static com.potatorider.domain.DeliveryStatus.ACCEPT;
+import static com.potatorider.domain.DeliveryStatus.PICKED_UP;
+import static com.potatorider.domain.DeliveryStatus.REQUEST;
+import static com.potatorider.domain.DeliveryStatus.RIDER_SET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.potatorider.deliveryinfoservice.domain.DeliverySteps;
+import com.potatorider.domain.Delivery;
+import com.potatorider.exception.DeliveryNotFountException;
+import com.potatorider.publisher.DeliveryPublisher;
+import com.potatorider.repository.DeliveryRepository;
+import com.potatorider.service.DeliveryService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+public class DeliveryServiceTests {
+
+    @InjectMocks
+    DeliveryService deliveryService;
+
+    @Mock
+    DeliveryRepository deliveryRepository;
+
+    @Mock
+    DeliveryPublisher deliveryPublisher;
+
+    @Nested
+    class Save_delivery {
+
+        @Test
+        void success() {
+            // Arrange
+            final Delivery delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(null);
+
+            when(deliveryRepository.save(isA(Delivery.class))).thenReturn(Mono.just(delivery));
+            when(deliveryPublisher.sendAddDeliveryEvent(isA(Delivery.class))).thenReturn(
+                Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.saveDelivery(delivery);
+
+            // Assert
+            StepVerifier.create(result).expectNext(delivery).verifyComplete();
+            verify(deliveryRepository, times(1)).save(delivery);
+            verify(deliveryPublisher, times(1)).sendAddDeliveryEvent(delivery);
+        }
+
+        @Test
+        void fail_delivery_is_null() {
+            // Arrange
+            when(deliveryRepository.save(any())).thenReturn(Mono.error(IllegalArgumentException::new));
+
+            // Act
+            var result = deliveryService.saveDelivery(null);
+
+            // Assert
+            StepVerifier.create(result).expectError(IllegalArgumentException.class).verify();
+            verify(deliveryRepository, times(1)).save(any());
+        }
+    }
+
+    @Nested
+    class Accept_delivery {
+
+        @Test
+        void success() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(REQUEST);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+            when(deliveryRepository.save(isA(Delivery.class))).thenReturn(Mono.just(delivery));
+            when(deliveryPublisher.sendSetRiderEvent(any())).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.acceptDelivery(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectNext(delivery).verifyComplete();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(1)).save(delivery);
+            verify(deliveryPublisher, times(1)).sendSetRiderEvent(delivery);
+        }
+
+        @Test
+        void fail_not_found() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.error(DeliveryNotFountException::new));
+
+            // Act
+            var result = deliveryService.acceptDelivery(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectError(DeliveryNotFountException.class).verify();
+            verify(deliveryRepository, times(0)).save(any());
+            verify(deliveryPublisher, times(0)).sendSetRiderEvent(any());
+        }
+
+        @Test
+        void fail_delivery_status_is_not_request() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(ACCEPT);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.acceptDelivery(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectError(IllegalStateException.class).verify();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(0)).save(any());
+            verify(deliveryPublisher, times(0)).sendSetRiderEvent(any());
+        }
+    }
+
+    @Nested
+    class Set_delivery_rider {
+
+        @Test
+        void success() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(ACCEPT);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+            when(deliveryRepository.save(delivery)).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.setDeliveryRider(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectNext(delivery).verifyComplete();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(1)).save(delivery);
+        }
+
+        @Test
+        void fail_delivery_status_is_not_accept() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(REQUEST);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.setDeliveryRider(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectError(IllegalStateException.class).verify();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(0)).save(any());
+        }
+    }
+
+    @Nested
+    class Pick_up_delivery {
+
+        @Test
+        void success() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(RIDER_SET);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+            when(deliveryRepository.save(delivery)).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.pickUpDelivery(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectNext(delivery).verifyComplete();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(1)).save(delivery);
+        }
+
+        @Test
+        void fail_delivery_status_is_not_set_rider() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(REQUEST);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.pickUpDelivery(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectError(IllegalStateException.class).verify();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(0)).save(any());
+        }
+    }
+
+    @Nested
+    class Complete_delivery {
+
+        @Test
+        void success() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(PICKED_UP);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+            when(deliveryRepository.save(delivery)).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.completeDelivery(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectNext(delivery).verifyComplete();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(1)).save(delivery);
+        }
+
+        @Test
+        void fail_delivery_status_is_not_picked_up() {
+            // Arrange
+            String deliveryId = "delivery-1234";
+            var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(REQUEST);
+
+            when(deliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+
+            // Act
+            var result = deliveryService.completeDelivery(deliveryId);
+
+            // Assert
+            StepVerifier.create(result).expectError(IllegalStateException.class).verify();
+            verify(deliveryRepository, times(1)).findById(deliveryId);
+            verify(deliveryRepository, times(0)).save(any());
+        }
+    }
+}
