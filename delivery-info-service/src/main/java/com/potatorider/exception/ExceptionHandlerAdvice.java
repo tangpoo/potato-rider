999a1eaece3b777f5f946bbad6fa6bea5873dfbe
tangpoo <lhinav3@gmail.com>
2024-07-11 20:48:01 +0900

    #38 Format: Delivery-Info

--- delivery-info-service/src/main/java/com/potatorider/exception/ExceptionHandlerAdvice.java
+++ delivery-info-service/src/main/java/com/potatorider/exception/ExceptionHandlerAdvice.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
