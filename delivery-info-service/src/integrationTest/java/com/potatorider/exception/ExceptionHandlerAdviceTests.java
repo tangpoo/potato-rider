848ed1306f1ca22e75c70d382630fb5d10304dac
tangpoo <lhinav3@gmail.com>
2024-07-11 19:43:52 +0900

    #27 Test: Delivery-Info ExceptionHandler Test 구현

---
+++ delivery-info-service/src/integrationTest/java/com/potatorider/exception/ExceptionHandlerAdviceTests.java
@@ -0,0 +1,81 @@
+package com.potatorider.exception;
+
+import static org.mockito.Mockito.*;
+
+import com.potatorider.controller.DeliveryController;
+import com.potatorider.controller.DeliveryControllerTests;
+import com.potatorider.deliveryinfoservice.domain.DeliverySteps;
+import com.potatorider.domain.Delivery;
+import com.potatorider.domain.DeliveryStatus;
+import com.potatorider.service.DeliveryService;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(DeliveryController.class)
+@AutoConfigureWebTestClient
+public class ExceptionHandlerAdviceTests {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @MockBean
+    DeliveryService deliveryService;
+
+    final String DELIVERY_URL = "/api/v1/delivery";
+
+    static Stream<Arguments> exceptionClassList() {
+        return Stream.of(
+            Arguments.of(new IllegalArgumentException(), HttpStatus.BAD_REQUEST),
+            Arguments.of(new IllegalStateException(), HttpStatus.BAD_REQUEST),
+            Arguments.of(new DeliveryNotFoundException(), HttpStatus.BAD_REQUEST),
+            Arguments.of(new RuntimeException(), HttpStatus.INTERNAL_SERVER_ERROR)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("exceptionClassList")
+    void exceptionHandlingTest(Throwable throwable, HttpStatus httpStatus) {
+        // Arrange
+        String deliveryId = "id-1234";
+
+        when(deliveryService.findDelivery(deliveryId)).thenThrow(throwable);
+
+        // Act + Assert
+        testClient
+            .get()
+            .uri(DELIVERY_URL + "/{deliveryId}", deliveryId)
+            .exchange()
+            .expectStatus()
+            .isEqualTo(httpStatus);
+    }
+
+    @Test
+    void handleWebExchangeBindingError() {
+        // Arrange
+        var delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(
+            DeliveryStatus.REQUEST);
+
+        // Act
+        testClient
+            .post()
+            .uri(DELIVERY_URL)
+            .bodyValue(new Delivery())
+            .exchange()
+            .expectStatus()
+            .isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        // Assert
+    }
+
+}
