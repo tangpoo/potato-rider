09156176eb5b1420640add401b676dbbd4b8693c
tangpoo <lhinav3@gmail.com>
2024-07-13 20:21:59 +0900

    #42 Build: Delivery-Relay 배송 요청 이벤트 수신 구현

--- delivery-relay-service/src/main/java/com/potatorider/repository/RelayRepository.java
+++ delivery-relay-service/src/main/java/com/potatorider/repository/RelayRepository.java
@@ -1,3 +1,8 @@
 package com.potatorider.repository;
 
-public interface RelayRepository {}
+import com.potatorider.domain.RelayRequest;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface RelayRepository extends ReactiveMongoRepository<RelayRequest, String> {
+
+}
