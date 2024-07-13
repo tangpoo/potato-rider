8abdec0093983a0364ea8f990d29b3ec1637d093
tangpoo <lhinav3@gmail.com>
2024-07-13 20:21:16 +0900

    #42 Build: Delivery-Relay Domain 구현

--- delivery-relay-service/src/main/java/com/potatorider/domain/RelayRequest.java
+++ delivery-relay-service/src/main/java/com/potatorider/domain/RelayRequest.java
@@ -1,3 +1,30 @@
 package com.potatorider.domain;
 
-public class RelayRequest {}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RelayRequest {
+
+    @Id
+    private String id;
+
+    private ReceiverType receiverType;
+
+    private String receiverId;
+
+    private Delivery delivery;
+
+    public RelayRequest(final ReceiverType receiverType, final String receiverId,
+        final Delivery delivery) {
+        this.receiverType = receiverType;
+        this.receiverId = receiverId;
+        this.delivery = delivery;
+    }
+}
