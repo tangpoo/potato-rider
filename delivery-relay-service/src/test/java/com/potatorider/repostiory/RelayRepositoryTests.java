3597a549bd8e2b92fb463eec4c6dc536b2df49e9
tangpoo <lhinav3@gmail.com>
2024-07-14 20:39:14 +0900

    #52 Test: Delivery-Relay MongoDB TestContainer 설정

--- delivery-relay-service/src/test/java/com/potatorider/repostiory/RelayRepositoryTests.java
+++ delivery-relay-service/src/test/java/com/potatorider/repostiory/RelayRepositoryTests.java
@@ -6,22 +6,35 @@ import com.potatorider.domain.Delivery;
 import com.potatorider.domain.ReceiverType;
 import com.potatorider.domain.RelayRequest;
 import com.potatorider.repository.RelayRepository;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.domain.PageRequest;
-
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.test.StepVerifier;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @DataMongoTest
+@Testcontainers
 public class RelayRepositoryTests {
 
-    @Autowired RelayRepository relayRepository;
+    @Autowired
+    RelayRepository relayRepository;
+
+    @Container
+    private static final MongoDBContainer mongoContainer =
+        new MongoDBContainer("mongodb/mongodb-community-server:latest");
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoContainer::getReplicaSetUrl);
+    }
 
     @AfterEach
     void tearDown() {
@@ -34,9 +47,9 @@ public class RelayRepositoryTests {
 
         for (int i = 0; i < 3; i++) {
             RelayRequest relayRequest1 =
-                    new RelayRequest(ReceiverType.SHOP, "shop-" + i, new Delivery());
+                new RelayRequest(ReceiverType.SHOP, "shop-" + i, new Delivery());
             RelayRequest relayRequest2 =
-                    new RelayRequest(ReceiverType.AGENCY, "agency-" + i, new Delivery());
+                new RelayRequest(ReceiverType.AGENCY, "agency-" + i, new Delivery());
             relayRequestList.add(relayRequest1);
             relayRequestList.add(relayRequest2);
         }
@@ -55,13 +68,13 @@ public class RelayRepositoryTests {
 
         // Act
         var result =
-                relayRepository.findAllByReceiverTypeContaining(pageRequest, ReceiverType.SHOP);
+            relayRepository.findAllByReceiverTypeContaining(pageRequest, ReceiverType.SHOP);
 
         // Assert
         StepVerifier.create(result)
-                .expectNextMatches(request -> request.getReceiverType() == ReceiverType.SHOP)
-                .expectNextMatches(request -> request.getReceiverType() == ReceiverType.SHOP)
-                .expectNextMatches(request -> request.getReceiverType() == ReceiverType.SHOP)
-                .verifyComplete();
+            .expectNextMatches(request -> request.getReceiverType() == ReceiverType.SHOP)
+            .expectNextMatches(request -> request.getReceiverType() == ReceiverType.SHOP)
+            .expectNextMatches(request -> request.getReceiverType() == ReceiverType.SHOP)
+            .verifyComplete();
     }
 }
