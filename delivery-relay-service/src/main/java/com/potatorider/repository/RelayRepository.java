837f73095c0f4b2dbc2e559de5f056ff5be9bcad
tangpoo <lhinav3@gmail.com>
2024-07-14 15:32:00 +0900

    #48 #49 Build: Delivery-Relay 배차 요청 조회 API, 대기 주문 조회 API 구현

--- delivery-relay-service/src/main/java/com/potatorider/repository/RelayRepository.java
+++ delivery-relay-service/src/main/java/com/potatorider/repository/RelayRepository.java
@@ -1,7 +1,12 @@
 package com.potatorider.repository;
 
+import com.potatorider.domain.ReceiverType;
 import com.potatorider.domain.RelayRequest;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
-public interface RelayRepository extends ReactiveMongoRepository<RelayRequest, String> {}
+public interface RelayRepository extends ReactiveMongoRepository<RelayRequest, String> {
+    Flux<RelayRequest> findAllByReceiverTypeContaining(Pageable pageable, ReceiverType receiverType);
+}
