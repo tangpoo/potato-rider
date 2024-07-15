3ceee769a52064a8fc66ab9e044b3f6e863abed3
tangpoo <lhinav3@gmail.com>
2024-07-15 13:50:53 +0900

    #56 Build: Rider-Location 구조화, Gradle 설정

---
+++ rider-location-service/src/main/java/com/potatorider/riderlocationservice/RiderLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.potatorider.riderlocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderLocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiderLocationServiceApplication.class, args);
+	}
+
+}
---
+++ rider-location-service/src/test/java/com/potatorider/riderlocationservice/RiderLocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatorider.riderlocationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderLocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
