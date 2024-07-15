c914c956998fac5b8b1238e26e0de236087f2596
tangpoo <lhinav3@gmail.com>
2024-07-15 17:53:58 +0900

    #60 Test: Rider-Location Service Tests

---
+++ rider-location-service/src/test/java/com/potatorider/domain/RiderLocationSteps.java
@@ -0,0 +1,9 @@
+package com.potatorider.domain;
+
+public class RiderLocationSteps {
+
+    public static RiderLocation createRiderLocation(){
+        return new RiderLocation("id-1234", "delivery-1234", 3.11f,
+            3.11f);
+    }
+}
