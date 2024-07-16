105e9bcffcf36864d572029acf4c248d7bcbcffa
tangpoo <131866367+tangpoo@users.noreply.github.com>
2024-07-16 17:20:34 +0900

    Rider location Controller Tests (#67)
    * #40 Docs: CI google-java-format.yml 생성
    
    * #40 Docs: CI delivery-info-service.yml 생성
    (단위 테스트, 통합 테스트, jacoco 커버리지 생성)
    
    * Docs: CI delivery-info-service.yml 생성
    (단위 테스트, 통합 테스트, jacoco 커버리지 생성)
    
    * #62 Test: Rider-Location Controller Tests
    
    * Format : Google Java Format
    
    ---------
    
    Co-authored-by: github-actions <>

--- delivery-info-service/src/integrationTest/java/com/potatorider/controller/DeliveryControllerTests.java
+++ delivery-info-service/src/integrationTest/java/com/potatorider/controller/DeliveryControllerTests.java
@@ -19,7 +19,6 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -33,7 +32,6 @@ import reactor.test.StepVerifier;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @Testcontainers
-@ContextConfiguration
 public class DeliveryControllerTests {
 
     @Autowired WebTestClient testClient;
