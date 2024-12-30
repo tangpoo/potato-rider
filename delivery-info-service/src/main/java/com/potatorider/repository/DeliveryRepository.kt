76d2f6401138b51d2d26b4d06a3cdf440f989a11
tangpoo <lhinav3@gmail.com>
2024-12-30 18:36:34 +0900

    #93 Refactor: DeliveryRepository 코틀린 변환

--- delivery-info-service/src/main/java/com/potatorider/repository/DeliveryRepository.kt
+++ delivery-info-service/src/main/java/com/potatorider/repository/DeliveryRepository.kt
@@ -1,13 +1,10 @@
-package com.potatorider.repository;
+package com.potatorider.repository
 
-import com.potatorider.domain.Delivery;
+import com.potatorider.domain.Delivery
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import reactor.core.publisher.Flux;
-
-public interface DeliveryRepository extends ReactiveMongoRepository<Delivery, String> {
-
-    Flux<Delivery> findAllBy(Pageable pageable);
+interface DeliveryRepository : ReactiveMongoRepository<Delivery, String> {
+    fun findAllBy(pageable: Pageable): Flux<Delivery>
 }
