d69613f8985edc3ce89c94bacacecd302d712e9c
tangpoo <lhinav3@gmail.com>
2024-12-30 16:20:14 +0900

    #93 Refactor: DeliveryValidator 클래스 코틀린 변환 및 statusIsExpected 함수 최상위 이동

--- delivery-info-service/src/main/java/com/potatorider/util/DeliveryValidator.kt
+++ delivery-info-service/src/main/java/com/potatorider/util/DeliveryValidator.kt
@@ -1,15 +1,14 @@
-package com.potatorider.util;
+package com.potatorider.util
 
-import com.potatorider.domain.Delivery;
-import com.potatorider.domain.DeliveryStatus;
-import reactor.core.publisher.Mono;
+import com.potatorider.domain.Delivery
+import com.potatorider.domain.DeliveryStatus
+import reactor.core.publisher.Mono
 
-public class DeliveryValidator {
-        public static Mono<Delivery> statusIsExpected(Delivery delivery, DeliveryStatus expected) {
-            return delivery.getDeliveryStatus().equals(expected)
-                    ? Mono.just(delivery)
-                    : Mono.error(
-                            new IllegalStateException(
-                                    String.format("주문 상태가 %s 가 아닙니다.", expected)));
-        }
+fun statusIsExpected(delivery: Delivery, expected: DeliveryStatus): Mono<Delivery> {
+    return if (delivery.deliveryStatus == expected) Mono.just(delivery)
+    else Mono.error(
+        IllegalStateException(
+            String.format("주문 상태가 %s 가 아닙니다.", expected)
+        )
+    )
 }
