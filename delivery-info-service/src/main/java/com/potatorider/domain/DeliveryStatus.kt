da7a02e0ec60a9d274f3dadf9e2d38e63c8b8aa8
tangpoo <lhinav3@gmail.com>
2024-12-30 16:11:28 +0900

    #93 Refactor: DeliveryStatus 클래스 코틀린 변환

--- delivery-info-service/src/main/java/com/potatorider/domain/DeliveryStatus.kt
+++ delivery-info-service/src/main/java/com/potatorider/domain/DeliveryStatus.kt
@@ -1,28 +1,23 @@
-package com.potatorider.domain;
+package com.potatorider.domain
 
-public enum DeliveryStatus {
+enum class DeliveryStatus(
+    private val shopPerspective: String,
+    private val customerPerspective: String
+) {
     REQUEST("주문요청", "주문요청"),
     ACCEPT("주문승인", "주문승인"),
     RIDER_SET("배차완료", "배차완료"),
     PICKED_UP("픽업완료", "픽업완료"),
     COMPLETE("배송완료", "배송완료");
 
-    private final String shopPerspective;
-    private final String customerPerspective;
-
-    DeliveryStatus(final String shopPerspective, final String customerPerspective) {
-        this.shopPerspective = shopPerspective;
-        this.customerPerspective = customerPerspective;
-    }
-
-    public DeliveryStatus getNext() {
-        if (this.isLast()) {
-            return COMPLETE;
+    val next: DeliveryStatus
+        get() {
+            if (this.isLast) {
+                return COMPLETE
+            }
+            return entries[ordinal + 1]
         }
-        return DeliveryStatus.values()[this.ordinal() + 1];
-    }
 
-    private boolean isLast() {
-        return DeliveryStatus.values().length - 1 == this.ordinal();
-    }
+    private val isLast: Boolean
+        get() = entries.size - 1 == this.ordinal
 }
