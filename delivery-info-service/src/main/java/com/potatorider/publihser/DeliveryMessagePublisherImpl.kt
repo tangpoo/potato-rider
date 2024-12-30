2e7b0a29ba5c89ec8e06595dff9a658e1de4822e
tangpoo <lhinav3@gmail.com>
2024-12-30 18:50:58 +0900

    #93 Refactor: DeliveryMessagePublisherImpl 상수 처리 및 람다 간결화

--- delivery-info-service/src/main/java/com/potatorider/publihser/DeliveryMessagePublisherImpl.kt
+++ delivery-info-service/src/main/java/com/potatorider/publihser/DeliveryMessagePublisherImpl.kt
@@ -1,12 +1,12 @@
 package com.potatorider.publihser
 
 import com.potatorider.domain.Delivery
-import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
 import org.springframework.amqp.core.AmqpTemplate
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
+import reactor.core.scheduler.Schedulers.*
 
 @Component
 @Slf4j
@@ -14,31 +14,33 @@ class DeliveryMessagePublisherImpl(
     private val messageQueue: AmqpTemplate
 ) : DeliveryPublisher {
 
-    private val shopExchange = "messageQueue.exchange.shop"
-    private val agencyExchange = "messageQueue.exchange.agency"
+    companion object {
+        private const val SHOP_EXCHANGE = "messageQueue.exchange.shop"
+        private const val AGENCY_EXCHANGE = "messageQueue.exchange.agency"
+    }
 
     override fun sendAddDeliveryEvent(delivery: Delivery): Mono<Delivery> {
         return Mono.just(delivery)
-            .subscribeOn(Schedulers.boundedElastic())
-            .flatMap { delivery -> this.publishAddDeliveryEvent(delivery) }
+            .subscribeOn(boundedElastic())
+            .flatMap(::publishAddDeliveryEvent)
     }
 
     override fun sendSetRiderEvent(delivery: Delivery): Mono<Delivery> {
         return Mono.just(delivery)
-            .subscribeOn(Schedulers.boundedElastic())
-            .flatMap { delivery -> this.publishSetRiderEvent(delivery) }
+            .subscribeOn(boundedElastic())
+            .flatMap(::publishSetRiderEvent)
     }
 
     private fun publishSetRiderEvent(delivery: Delivery): Mono<Delivery> {
         return Mono.fromCallable {
-            messageQueue.convertAndSend(agencyExchange, "setRider", delivery)
+            messageQueue.convertAndSend(AGENCY_EXCHANGE, "setRider", delivery)
             delivery
         }
     }
 
     private fun publishAddDeliveryEvent(delivery: Delivery): Mono<Delivery> {
         return Mono.fromCallable {
-            messageQueue.convertAndSend(shopExchange, "addDelivery", delivery)
+            messageQueue.convertAndSend(SHOP_EXCHANGE, "addDelivery", delivery)
             delivery
         }
     }
