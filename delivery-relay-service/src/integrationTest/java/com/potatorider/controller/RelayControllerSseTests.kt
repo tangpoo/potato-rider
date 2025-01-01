6aa3957c19d857d19308f2739cb9380f8eb51847
tangpoo <lhinav3@gmail.com>
2025-01-01 19:28:05 +0900

    #93 Refactor: Delivery-Relay RelayControllerSseTests 코틀린 변환

--- delivery-relay-service/src/integrationTest/java/com/potatorider/controller/RelayControllerSseTests.kt
+++ delivery-relay-service/src/integrationTest/java/com/potatorider/controller/RelayControllerSseTests.kt
@@ -1,131 +1,138 @@
-package com.potatorider.controller;
+package com.potatorider.controller
 
-import com.potatorider.domain.Delivery;
-import com.potatorider.domain.DeliveryStatus;
-import com.potatorider.domain.ReceiverType;
-import com.potatorider.domain.RelayRequest;
-import com.potatorider.repository.RelayRepository;
-import com.potatorider.service.RelayService;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.RabbitMQContainer;
-import org.testcontainers.junit.jupiter.Container;
-
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
-
-import java.time.LocalDateTime;
+import com.potatorider.domain.Delivery
+import com.potatorider.domain.DeliveryStatus
+import com.potatorider.domain.ReceiverType
+import com.potatorider.domain.RelayRequest
+import com.potatorider.repository.RelayRepository
+import com.potatorider.service.RelayService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.codec.ServerSentEvent
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.containers.RabbitMQContainer
+import org.testcontainers.junit.jupiter.Container
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+import java.time.LocalDateTime
 
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class RelayControllerSseTests {
+class RelayControllerSseTests {
+    @LocalServerPort
+    private val port = 0
 
-    @Container
-    private static final RabbitMQContainer rabbitmqContainer =
-            new RabbitMQContainer("rabbitmq:latest");
+    @Autowired
+    lateinit var testClient: WebTestClient
 
-    @Container
-    private static final MongoDBContainer mongoContainer =
-            new MongoDBContainer("mongodb/mongodb-community-server:latest");
+    @Autowired
+    lateinit var relayService: RelayService
 
-    @LocalServerPort private int port;
-    @Autowired private WebTestClient testClient;
-    @Autowired private RelayService relayService;
-    @Autowired private RelayRepository relayRepository;
-
-    @DynamicPropertySource
-    static void configure(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoContainer::getReplicaSetUrl);
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-        rabbitmqContainer.start();
-        mongoContainer.start();
-    }
+    @Autowired
+    lateinit var relayRepository: RelayRepository
 
     @AfterEach
-    void tearDown() {
-        relayRepository.deleteAll().block();
+    fun tearDown() {
+        relayRepository.deleteAll().block()
     }
 
     @Test
-    public void test_sse_end_point() {
+    fun test_sse_end_point() {
         // Arrange
-        String url = "http://localhost:" + port + "/api/v1/relay/stream";
-        String receiverId = "shop-1234";
+        val url = "http://localhost:$port/api/v1/relay/stream"
+        val receiverId = "shop-1234"
 
-        final Delivery delivery = createDelivery();
+        val delivery = createDelivery()
 
-        relayService.saveDelivery(delivery, ReceiverType.SHOP).block();
+        relayService.saveDelivery(delivery, ReceiverType.SHOP).block()
 
         // Act
-        Flux<ServerSentEvent<RelayRequest>> eventFlux =
-                testClient
-                        .get()
-                        .uri(url)
-                        .header("Receiver-ID", receiverId)
-                        .exchange()
-                        .expectStatus()
-                        .isOk()
-                        .returnResult(
-                                new ParameterizedTypeReference<ServerSentEvent<RelayRequest>>() {})
-                        .getResponseBody();
+        val eventFlux: Flux<ServerSentEvent<RelayRequest>> =
+            testClient
+                .get()
+                .uri(url)
+                .header("Receiver-ID", receiverId)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult<ServerSentEvent<RelayRequest>>(
+                    object : ParameterizedTypeReference<ServerSentEvent<RelayRequest>>() {})
+                .getResponseBody()
 
         // Assert
         StepVerifier.create(eventFlux)
-                .expectNextMatches(
-                        event -> {
-                            RelayRequest relayRequest = event.data();
-                            return relayRequest != null
-                                    && relayRequest
-                                            .getDelivery()
-                                            .getOrderId()
-                                            .equals(delivery.getOrderId());
-                        })
-                .thenCancel()
-                .verify();
+            .expectNextMatches { event: ServerSentEvent<RelayRequest> ->
+                val relayRequest = event.data()
+                (relayRequest != null
+                        && (relayRequest
+                    .delivery
+                    .orderId
+                        == delivery.orderId))
+            }
+            .thenCancel()
+            .verify()
     }
 
-    private Delivery createDelivery() {
-        String id = "id-1234";
-        String orderId = "order1";
-        String riderId = "rider-1234";
-        String agencyId = "agency-1234";
-        String shopId = "shop-1234";
-        String customerId = "customer-1234";
-        String address = "충주시 호반로...";
-        String phoneNumber = "01011112222";
-        LocalDateTime orderTime = LocalDateTime.now().minusMinutes(1);
-        LocalDateTime pickupTime = LocalDateTime.now().plusMinutes(10);
-        LocalDateTime finishTime = LocalDateTime.now().plusMinutes(30);
+    private fun createDelivery(): Delivery {
+        val id = "id-1234"
+        val orderId = "order1"
+        val riderId = "rider-1234"
+        val agencyId = "agency-1234"
+        val shopId = "shop-1234"
+        val customerId = "customer-1234"
+        val address = "충주시 호반로..."
+        val phoneNumber = "01011112222"
+        val orderTime = LocalDateTime.now().minusMinutes(1)
+        val pickupTime = LocalDateTime.now().plusMinutes(10)
+        val finishTime = LocalDateTime.now().plusMinutes(30)
 
-        return new Delivery(
-                id,
-                orderId,
-                riderId,
-                agencyId,
-                shopId,
-                customerId,
-                address,
-                phoneNumber,
-                "",
-                DeliveryStatus.REQUEST,
-                orderTime,
-                pickupTime,
-                finishTime);
+        return Delivery(
+            id,
+            orderId,
+            riderId,
+            agencyId,
+            shopId,
+            customerId,
+            address,
+            phoneNumber,
+            "",
+            DeliveryStatus.REQUEST,
+            orderTime,
+            pickupTime,
+            finishTime
+        )
+    }
+
+    companion object {
+        @Container
+        @JvmStatic
+        private val rabbitmqContainer = RabbitMQContainer("rabbitmq:latest")
+
+        @Container
+        @JvmStatic
+        private val mongoContainer = MongoDBContainer("mongodb/mongodb-community-server:latest")
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun configure(registry: DynamicPropertyRegistry) {
+            registry.add("spring.data.mongodb.uri") { mongoContainer.replicaSetUrl }
+        }
+
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll() {
+            rabbitmqContainer.start()
+            mongoContainer.start()
+        }
     }
 }
