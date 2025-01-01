a2e2869623739e4ccc9d35d8b210212c7e7fd728
tangpoo <lhinav3@gmail.com>
2025-01-01 18:37:46 +0900

    #93 Refactor: Delivery-Relay RelayRequest 코틀린 변환

--- delivery-relay-service/src/main/java/com/potatorider/domain/RelayRequest.kt
+++ delivery-relay-service/src/main/java/com/potatorider/domain/RelayRequest.kt
@@ -1,34 +1,16 @@
-package com.potatorider.domain;
+package com.potatorider.domain
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-@Getter
-@NoArgsConstructor
-public class RelayRequest {
-
-    @Id private String id;
-
-    private ReceiverType receiverType;
-
-    private String receiverId;
-
-    private Delivery delivery;
-
-    private boolean isAccepted;
-
-    private boolean isEnabled;
-
-    public RelayRequest(
-            final ReceiverType receiverType, final String receiverId, final Delivery delivery) {
-        this.receiverType = receiverType;
-        this.receiverId = receiverId;
-        this.delivery = delivery;
-        this.isAccepted = false;
-        this.isEnabled = true;
-    }
+class RelayRequest(
+    val receiverType: ReceiverType,
+    val receiverId: String,
+    val delivery: Delivery
+) {
+    @Id
+    val id: String? = null
+    val isAccepted = false
+    val isEnabled = true
 }
