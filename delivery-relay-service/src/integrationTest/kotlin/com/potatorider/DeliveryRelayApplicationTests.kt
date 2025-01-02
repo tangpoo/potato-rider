104d51ac7106be7aab2ee31b6e6c19df1ae8edba
tangpoo <lhinav3@gmail.com>
2025-01-02 21:28:29 +0900

    #93 Refactor: CI/CD 에러 테스트 / 탐색 경로에 클래스 생성

---
+++ delivery-relay-service/src/integrationTest/kotlin/com/potatorider/DeliveryRelayApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.potatorider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliveryRelayApplicationTests {
+    @Test
+    fun contextLoads() {
+
+    }
+}
