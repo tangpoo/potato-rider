21fcf607fa552adda9cfccb5fc4de092ad698a32
tangpoo <lhinav3@gmail.com>
2025-01-06 16:06:18 +0900

    #93 Refactor: Delivery-Info DeliveryTests 테스트 코틀린 변환

--- delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/domain/DeliveryTests.kt
+++ delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/domain/DeliveryTests.kt
@@ -1,38 +1,34 @@
-package com.potatorider.deliveryinfoservice.domain;
+package com.potatorider.deliveryinfoservice.domain
 
-import static com.potatorider.domain.DeliveryStatus.ACCEPT;
-import static com.potatorider.domain.DeliveryStatus.COMPLETE;
-import static com.potatorider.domain.DeliveryStatus.REQUEST;
+import com.potatorider.deliveryinfoservice.domain.DeliverySteps.makeValidDeliveryWithDeliveryStatus
+import com.potatorider.domain.DeliveryStatus
+import com.potatorider.domain.DeliveryStatus.*
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.potatorider.domain.Delivery;
-
-import org.junit.jupiter.api.Test;
-
-public class DeliveryTests {
-
+class DeliveryTests {
     @Test
-    void next_status_is_not_last() {
+    fun next_status_is_not_last() {
         // Arrange
-        Delivery delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(REQUEST);
+        val delivery = makeValidDeliveryWithDeliveryStatus(REQUEST)
 
         // Act
-        final Delivery response = delivery.nextStatus();
+        val response = delivery.nextStatus()
 
         // Assert
-        assertThat(response.getDeliveryStatus()).isEqualTo(ACCEPT);
+        assertThat(response.deliveryStatus).isEqualTo(ACCEPT)
     }
 
     @Test
-    void next_status_is_last() {
+    fun next_status_is_last() {
         // Arrange
-        Delivery delivery = DeliverySteps.makeValidDeliveryWithDeliveryStatus(COMPLETE);
+        val delivery = makeValidDeliveryWithDeliveryStatus(COMPLETE)
 
         // Act
-        final Delivery response = delivery.nextStatus();
+        val response = delivery.nextStatus()
 
         // Assert
-        assertThat(response.getDeliveryStatus()).isEqualTo(COMPLETE);
+        assertThat(response.deliveryStatus).isEqualTo(COMPLETE)
     }
 }
