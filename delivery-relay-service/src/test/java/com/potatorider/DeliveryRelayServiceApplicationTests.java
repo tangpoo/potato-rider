c9f3ded00809f177730e03346d3288d19fd2bd98
tangpoo <lhinav3@gmail.com>
2025-01-06 15:54:44 +0900

    Revert "#93 Refactor: Rider-Location Kotlin Mockito 추가 / RiderLocationControllerTests의 DeliveryRepositoryImpl에 WireMock 동적 포트를 할당하고 Mocking 하기 위해 Spring Context로 관리하지 않고 직접 초기화 후 spy로 감싼다."
    This reverts commit 5a42ac4043b033d05bdcebb1f7518cbafb242b53.

--- delivery-relay-service/src/test/java/com/potatorider/DeliveryRelayServiceApplicationTests.java
+++ delivery-relay-service/src/test/java/com/potatorider/DeliveryRelayServiceApplicationTests.java
@@ -1,12 +1,12 @@
-// package com.potatorider;
+//package com.potatorider;
 //
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 //
-// @SpringBootTest
-// class DeliveryRelayServiceApplicationTests {
+//@SpringBootTest
+//class DeliveryRelayServiceApplicationTests {
 //
 //    @Test
 //    void contextLoads() {
 //    }
-// }
+//}
