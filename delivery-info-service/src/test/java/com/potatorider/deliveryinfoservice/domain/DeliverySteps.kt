dd33b6a216ac7f1f13d97ff59665f2aca5ff0827
tangpoo <lhinav3@gmail.com>
2025-01-06 16:05:38 +0900

    #93 Refactor: Delivery-Info DeliverySteps 테스트 코틀린 변환

--- delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/domain/DeliverySteps.kt
+++ delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/domain/DeliverySteps.kt
@@ -1,38 +1,38 @@
-package com.potatorider.deliveryinfoservice.domain;
+package com.potatorider.deliveryinfoservice.domain
 
-import com.potatorider.domain.Delivery;
-import com.potatorider.domain.DeliveryStatus;
+import com.potatorider.domain.Delivery
+import com.potatorider.domain.DeliveryStatus
+import java.time.LocalDateTime
 
-import java.time.LocalDateTime;
+object DeliverySteps {
+    @JvmStatic
+    fun makeValidDeliveryWithDeliveryStatus(deliveryStatus: DeliveryStatus?): Delivery {
+        val id = "id-1234"
+        val orderId = "order1"
+        val riderId = "rider-1234"
+        val agencyId = "agency-1234"
+        val shopId = "shop-1234"
+        val customerId = "customer-1234"
+        val address = "충주시 호반로..."
+        val phoneNumber = "01011112222"
+        val orderTime = LocalDateTime.now().minusMinutes(1)
+        val pickupTime = LocalDateTime.now().plusMinutes(10)
+        val finishTime = LocalDateTime.now().plusMinutes(30)
 
-public class DeliverySteps {
-
-    public static Delivery makeValidDeliveryWithDeliveryStatus(DeliveryStatus deliveryStatus) {
-        String id = "id-1234";
-        String orderId = "order1";
-        String riderId = "rider-1234";
-        String agencyId = "agency-1234";
-        String shopId = "shop-1234";
-        String customerId = "customer-1234";
-        String address = "충주시 호반로...";
-        String phoneNumber = "01011112222";
-        LocalDateTime orderTime = LocalDateTime.now().minusMinutes(1);
-        LocalDateTime pickupTime = LocalDateTime.now().plusMinutes(10);
-        LocalDateTime finishTime = LocalDateTime.now().plusMinutes(30);
-
-        return new Delivery(
-                id,
-                orderId,
-                riderId,
-                agencyId,
-                shopId,
-                customerId,
-                address,
-                phoneNumber,
-                "",
-                deliveryStatus,
-                orderTime,
-                pickupTime,
-                finishTime);
+        return Delivery(
+            id,
+            orderId,
+            riderId,
+            agencyId,
+            shopId,
+            customerId,
+            address,
+            phoneNumber,
+            "",
+            deliveryStatus,
+            orderTime,
+            pickupTime,
+            finishTime
+        )
     }
 }
