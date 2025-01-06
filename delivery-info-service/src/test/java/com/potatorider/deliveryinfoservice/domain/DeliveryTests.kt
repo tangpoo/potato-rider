9af934db2e72576334315cc6601d8b3f8ead7267
tangpoo <131866367+tangpoo@users.noreply.github.com>
2025-01-06 16:43:45 +0900

    Delivery-info 단위 테스트 코틀린 리팩터링 (#101)
    * #93 Refactor: 주석 추가 + 불필요한 @Document 제거
    
    * #93 Refactor: IDE 자동기능 사용해 Kotlin 플러그인 추가. / Delivery-Info 에서 추가한 버전과의 차이점을 조사 필요.
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Relay Delivery 코틀린 변환 / 테스트 일시적 주석처리
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Relay 단위 테스트 코틀린 변환
    
    * #93 Refactor: Delivery-Relay 함수타입 mock 의존성 추가 및 적용
    
    * #93 Refactor: Delivery-Relay contextLoads 테스트 주석 처리 : 단위 테스트를 가볍게 반복 수행하기 위한 결정.
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Relay DeliveryStatus 코틀린 변환
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Relay ReceiverType 코틀린 변환
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Relay RelayRequest 코틀린 변환
    
    * #93 Refactor: Delivery-Relay 테스트 sourceSets 수정 / 통합 테스트 통과 x
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Relay RelayControllerTests 코틀린 변환 / DeliverySteps 추출 / MongoDB의 리플렉션을 위해 RelayRequest 가변 필드 변경 / 테스트 컨테이너의 리플렉션을 위한 의존성 추가
    
    * #93 Refactor: Conflict 해결 커밋
    
    * #93 Refactor: Conflict 해결 커밋
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Info DeliveryStatusTests 테스트 코틀린 변환
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Info DeliverySteps 테스트 코틀린 변환
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Info DeliveryTests 테스트 코틀린 변환
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Info DeliveryMessagePublisherImplTests 테스트 코틀린 변환
    
    * .java에서 .kt로 이름을 변경합니다.
    
    * #93 Refactor: Delivery-Info DeliveryServiceTests 테스트 코틀린 변환 / Mockito Kotlin 적용
    
    * #93 Refactor: Delivery-Info 테스트 클래스 명 변경

---
+++ delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/domain/DeliveryTests.kt
@@ -0,0 +1,34 @@
+package com.potatorider.deliveryinfoservice.domain
+
+import com.potatorider.deliveryinfoservice.domain.DeliverySteps.makeValidDeliveryWithDeliveryStatus
+import com.potatorider.domain.DeliveryStatus
+import com.potatorider.domain.DeliveryStatus.*
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DeliveryTests {
+    @Test
+    fun next_status_is_not_last() {
+        // Arrange
+        val delivery = makeValidDeliveryWithDeliveryStatus(REQUEST)
+
+        // Act
+        val response = delivery.nextStatus()
+
+        // Assert
+        assertThat(response.deliveryStatus).isEqualTo(ACCEPT)
+    }
+
+    @Test
+    fun next_status_is_last() {
+        // Arrange
+        val delivery = makeValidDeliveryWithDeliveryStatus(COMPLETE)
+
+        // Act
+        val response = delivery.nextStatus()
+
+        // Assert
+        assertThat(response.deliveryStatus).isEqualTo(COMPLETE)
+    }
+}
---
+++ delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/publisher/DeliveryMessagePublisherImplTests.kt
@@ -0,0 +1,66 @@
+package com.potatorider.deliveryinfoservice.publisher
+
+import com.potatorider.deliveryinfoservice.domain.DeliverySteps.makeValidDeliveryWithDeliveryStatus
+import com.potatorider.domain.Delivery
+import com.potatorider.domain.DeliveryStatus
+import com.potatorider.publihser.DeliveryMessagePublisherImpl
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.*
+import org.mockito.Mockito
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.amqp.core.AmqpTemplate
+import reactor.test.StepVerifier
+
+@ExtendWith(MockitoExtension::class)
+class DeliveryMessagePublisherImplTests {
+    private val shopExchange = "messageQueue.exchange.shop"
+    private val agencyExchange = "messageQueue.exchange.agency"
+    private lateinit var deliveryPublisher: DeliveryMessagePublisherImpl
+    private lateinit var amqpTemplate: AmqpTemplate
+
+    @BeforeEach
+    fun setUp() {
+        amqpTemplate = mock(AmqpTemplate::class.java)
+        deliveryPublisher = DeliveryMessagePublisherImpl(amqpTemplate)
+    }
+
+    @Test
+    fun send_add_delivery_event() {
+        // Arrange
+        val delivery = makeValidDeliveryWithDeliveryStatus(DeliveryStatus.REQUEST)
+
+        // Act
+        val result = deliveryPublisher.sendAddDeliveryEvent(delivery)
+
+        // Assert
+        StepVerifier.create(result).expectNext(delivery).verifyComplete()
+        verify(amqpTemplate, times(1))
+            .convertAndSend(
+                eq(shopExchange), any(
+                    String::class.java
+                ), any(Delivery::class.java)
+            )
+    }
+
+    @Test
+    fun send_set_rider_event() {
+        // Arrange
+        val delivery = makeValidDeliveryWithDeliveryStatus(DeliveryStatus.ACCEPT)
+
+        // Act
+        val result = deliveryPublisher.sendSetRiderEvent(delivery)
+
+        // Assert
+        StepVerifier.create(result).expectNext(delivery).verifyComplete()
+        verify(amqpTemplate, times(1))
+            .convertAndSend(
+                eq(agencyExchange), any(
+                    String::class.java
+                ), any(Delivery::class.java)
+            )
+    }
+}
