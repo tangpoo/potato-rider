d6bdf1a3b640b4f22cc00421ecf1746c850c875a
tangpoo <lhinav3@gmail.com>
2025-01-07 20:53:25 +0900

    #93 Refactor: Delivery-Info SwaggerConfig 코틀린 변환

--- delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/publisher/DeliveryMessagePublisherImplTests.kt
+++ delivery-info-service/src/test/java/com/potatorider/deliveryinfoservice/publisher/DeliveryMessagePublisherImplTests.kt
@@ -7,7 +7,6 @@ import com.potatorider.publihser.DeliveryMessagePublisherImpl
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mockito
 import org.mockito.Mockito.*
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.amqp.core.AmqpTemplate
