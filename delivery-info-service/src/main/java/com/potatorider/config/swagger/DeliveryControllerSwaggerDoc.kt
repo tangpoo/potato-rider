9efd38fd095e44ce86d7d1d286bb96fd27e65c03
tangpoo <lhinav3@gmail.com>
2025-01-07 20:55:07 +0900

    #93 Refactor: Delivery-Info DeliveryControllerSwaggerDoc null 가능성 제거

--- delivery-info-service/src/main/java/com/potatorider/config/swagger/DeliveryControllerSwaggerDoc.kt
+++ delivery-info-service/src/main/java/com/potatorider/config/swagger/DeliveryControllerSwaggerDoc.kt
@@ -9,26 +9,26 @@ import reactor.core.publisher.Mono
 @Tag(name = "Delivery API")
 interface DeliveryControllerSwaggerDoc {
     @Operation(summary = "신규배송 저장")
-    fun saveDelivery(delivery: Delivery?): Mono<Delivery?>?
+    fun saveDelivery(delivery: Delivery): Mono<Delivery>
 
     @Operation(summary = "배송 승인")
-    fun acceptDelivery(deliveryId: String?): Mono<Delivery?>?
+    fun acceptDelivery(deliveryId: String): Mono<Delivery>
 
     @Operation(summary = "배차 승인")
-    fun setDeliveryRider(deliveryId: String?): Mono<Delivery?>?
+    fun setDeliveryRider(deliveryId: String): Mono<Delivery>
 
     @Operation(summary = "상품 픽업")
-    fun pickUpDelivery(deliveryId: String?): Mono<Delivery?>?
+    fun pickUpDelivery(deliveryId: String): Mono<Delivery>
 
     @Operation(summary = "배송 완료")
-    fun completeDelivery(deliveryId: String?): Mono<Delivery?>?
+    fun completeDelivery(deliveryId: String): Mono<Delivery>
 
     @Operation(summary = "배송 조회")
-    fun findDelivery(deliverId: String?): Mono<Delivery?>?
+    fun findDelivery(deliverId: String): Mono<Delivery>
 
     @Operation(summary = "전체 배송 조회")
-    fun findAllDelivery(page: Int, size: Int): Flux<Delivery?>?
+    fun findAllDelivery(page: Int, size: Int): Flux<Delivery>
 
     @Operation(summary = "배송 픽업 확인")
-    fun isPickedUp(deliveryId: String?): Mono<Boolean?>?
+    fun isPickedUp(deliveryId: String): Mono<Boolean>
 }
