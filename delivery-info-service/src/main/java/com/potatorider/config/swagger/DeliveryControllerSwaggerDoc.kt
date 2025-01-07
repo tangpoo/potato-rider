cc588f0cae9b2befe6f7d1b1d2979393c0c6cce3
tangpoo <lhinav3@gmail.com>
2025-01-07 20:53:41 +0900

    #93 Refactor: Delivery-Info DeliveryControllerSwaggerDoc 코틀린 변환

--- delivery-info-service/src/main/java/com/potatorider/config/swagger/DeliveryControllerSwaggerDoc.kt
+++ delivery-info-service/src/main/java/com/potatorider/config/swagger/DeliveryControllerSwaggerDoc.kt
@@ -1,37 +1,34 @@
-package com.potatorider.config.swagger;
+package com.potatorider.config.swagger
 
-import com.potatorider.domain.Delivery;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import com.potatorider.domain.Delivery
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Tag(name = "Delivery API")
-public interface DeliveryControllerSwaggerDoc {
-
+interface DeliveryControllerSwaggerDoc {
     @Operation(summary = "신규배송 저장")
-    Mono<Delivery> saveDelivery(Delivery delivery);
+    fun saveDelivery(delivery: Delivery?): Mono<Delivery?>?
 
     @Operation(summary = "배송 승인")
-    Mono<Delivery> acceptDelivery(String deliveryId);
+    fun acceptDelivery(deliveryId: String?): Mono<Delivery?>?
 
     @Operation(summary = "배차 승인")
-    Mono<Delivery> setDeliveryRider(String deliveryId);
+    fun setDeliveryRider(deliveryId: String?): Mono<Delivery?>?
 
     @Operation(summary = "상품 픽업")
-    Mono<Delivery> pickUpDelivery(String deliveryId);
+    fun pickUpDelivery(deliveryId: String?): Mono<Delivery?>?
 
     @Operation(summary = "배송 완료")
-    Mono<Delivery> completeDelivery(String deliveryId);
+    fun completeDelivery(deliveryId: String?): Mono<Delivery?>?
 
     @Operation(summary = "배송 조회")
-    Mono<Delivery> findDelivery(String deliverId);
+    fun findDelivery(deliverId: String?): Mono<Delivery?>?
 
     @Operation(summary = "전체 배송 조회")
-    Flux<Delivery> findAllDelivery(int page, int size);
+    fun findAllDelivery(page: Int, size: Int): Flux<Delivery?>?
 
     @Operation(summary = "배송 픽업 확인")
-    Mono<Boolean> isPickedUp(String deliveryId);
+    fun isPickedUp(deliveryId: String?): Mono<Boolean?>?
 }
